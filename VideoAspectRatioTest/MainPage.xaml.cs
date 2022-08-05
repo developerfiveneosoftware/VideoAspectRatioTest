1c02083e136bbceb0fe169619501e56bf55f2570
developerfiveneosoftware <developerfive@neosoftware.uk>
2022-08-05 16:34:57 +0100

    Add project files.

---
+++ VideoAspectRatioTest/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace VideoAspectRatioTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnAppearing(object sender, System.EventArgs e)
+        {
+            base.OnAppearing();
+            ((MainViewModel)BindingContext).OnAppearing();
+        }
+
+        void OnDisappearing(object sender, System.EventArgs e)
+        {
+            base.OnDisappearing();
+            ((MainViewModel)BindingContext).OnDisappearing();
+        }
+    }
+}
